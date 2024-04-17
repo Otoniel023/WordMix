887af3a58d42e221cc07118b05386a4f1ebc0519
Otoniel Martinez <138731108+Otoniel023@users.noreply.github.com>
2024-04-17 14:08:00 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string palabra = "Otoniel";
+        ImprimirCombinaciones(palabra);
+    }
+
+    static void ImprimirCombinaciones(string palabra)
+    {
+        ImprimirCombinacionesAuxiliar("", palabra);
+    }
+
+    static void ImprimirCombinacionesAuxiliar(string prefijo, string sufijo)
+    {
+        int n = sufijo.Length;
+        if (n == 0)
+        {
+            Console.WriteLine(prefijo);
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ImprimirCombinacionesAuxiliar(prefijo + sufijo[i], sufijo.Substring(0, i) + sufijo.Substring(i + 1, n - i - 1));
+            }
+        }
+    }
+}
